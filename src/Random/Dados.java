c5c71a92f646f95f094115308dfd823e9c28aeb7
macram <macram@macram.es>
2012-07-30 03:11:50 +0200

    Añado documentación interna.

--- src/Random/Dados.java
+++ src/Random/Dados.java
@@ -3,18 +3,20 @@ package Random;
 import java.util.ArrayList;
 
 public class Dados {
-	/*
-	 * En SHI, al realizar la ficha de PJ se repiten las tiradas de habilidades con valor menor
-	 * a 40 hasta un máximo de tres veces (la tercera repetición se toma sí o sí). Cambiar valor
-	 * si procede, o en su lugar, poner null si no se aplica esta regla
+
+	/* Permite simular la tirada de un único dado de valor máximo RANGO.
+	 * Por ejemplo, para simular la tirada de un dado de diez caras el método será llamado
+	 * de la siguiente manera: "tirarDado(10);".
 	 */
-	
 	public static int tirarDado(int rango) {
 		double resultado;
 		resultado=Math.random()*rango; // Se le suma 1 si el valor del 0 es 10. 
 		return (int)resultado;
 	}
 	
+	/* Simula el lanzamiento de un conjunto "NUMERO" de dados, con un valor máximo de RANGO. 
+	 * El resultado se devuelve en un array. 
+	 */
 	public static ArrayList<Integer> tirarDados(int numero, int rango) {
 		int dado,vecesTiradas;
 		ArrayList<Integer> resultados=new ArrayList<Integer>();
@@ -24,7 +26,14 @@ public class Dados {
 		return resultados;
 	}
 	
-	public static ArrayList<Integer> tirarDados(int numero, int rango, int valor) {
+	/*
+	 * En SHI, al realizar la ficha de PJ se repiten las tiradas de habilidades con valor menor
+	 * a 40 hasta un máximo de tres veces (la tercera repetición se toma sí o sí). Cambiar valor
+	 * si procede, o en su lugar, poner null si no se aplica esta regla.
+	 * Este método realiza NÚMERO tiradas de dados de RANGO caras, y si un valor es menor a VALORMINIMO,
+	 * se repite hasta un máximo de 3 veces (esto último está hardcodeado). 
+	 */	
+	public static ArrayList<Integer> tirarDados(int numero, int rango, int valorMinimo) {
 		int dado,vecesTiradas;
 		ArrayList<Integer> resultados=new ArrayList<Integer>();
 		for (int i=0; i<numero; i++) {
@@ -33,7 +42,7 @@ public class Dados {
 				dado=tirarDado(rango);
 				vecesTiradas++;
 			}
-			while (dado<valor && vecesTiradas<3);
+			while (dado<valorMinimo && vecesTiradas<3);
 			resultados.add(dado);
 		}
 		return resultados;
